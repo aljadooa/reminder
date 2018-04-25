f1f664c6e0b5cb82293803e23b2024c44ca292dd
Hassan <haljadooa@gmail.com>
2018-04-24 18:06:05 -0700

    updated reminder.sql

--- database/reminder.sql
+++ database/reminder.sql
@@ -11,6 +11,23 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Dumping database structure for reminder
+CREATE DATABASE IF NOT EXISTS `reminder` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `reminder`;
+
+-- Dumping structure for table reminder.reminders
+CREATE TABLE IF NOT EXISTS `reminders` (
+  `id` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL DEFAULT '0',
+  `reminder` varchar(10000) DEFAULT '0',
+  `date` varchar(50) NOT NULL DEFAULT '0',
+  `completed` varchar(3) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
